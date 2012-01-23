20ff5c0db393714403aced16d09b91760580934f
Thomas Adamcik <thomas@adamcik.no>
2012-01-23 17:43:17 +0100

    Always spawn windows as slaves

--- awesome.lua
+++ awesome.lua
@@ -302,6 +302,9 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
+    { rule = { },
+      properties = { },
+      callback = awful.client.setslave },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
