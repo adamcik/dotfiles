70fbaf531ab31d491164e6fe1915866121139873
Thomas Adamcik <thomas@adamcik.no>
2012-01-23 18:18:55 +0100

    Skip gconf stuff if gconf is missing

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ done
 test -d ~/.config/awesome        || mkdir -p ~/.config/awesome
 test -f ~/.config/awesome/rc.lua || ln -s $BASEDIR/awesome.lua ~/.config/awesome/rc.lua
 
-gconftool-2 --type string --set /apps/gnome_settings_daemon/keybindings/screensaver XF86ScreenSaver
-gconftool-2 --type string --set /apps/gnome_settings_daemon/keybindings/volume_down XF86AudioLowerVolume
-gconftool-2 --type string --set /apps/gnome_settings_daemon/keybindings/volume_up   XF86AudioRaiseVolume
+if [ -x $(which gconftool-2) ]; then
+  gconftool-2 --type string --set /apps/gnome_settings_daemon/keybindings/screensaver XF86ScreenSaver
+  gconftool-2 --type string --set /apps/gnome_settings_daemon/keybindings/volume_down XF86AudioLowerVolume
+  gconftool-2 --type string --set /apps/gnome_settings_daemon/keybindings/volume_up   XF86AudioRaiseVolume
+fi
