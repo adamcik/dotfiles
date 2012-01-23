82b1c16c51be9bd583c1df9186b04f419f3212ca
Thomas Adamcik <thomas@adamcik.no>
2012-01-23 18:05:09 +0100

    Set gnome keybindings during install.

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ done
 
 test -d ~/.config/awesome        || mkdir -p ~/.config/awesome
 test -f ~/.config/awesome/rc.lua || ln -s $BASEDIR/awesome.lua ~/.config/awesome/rc.lua
+
+gconftool-2 --type string --set /apps/gnome_settings_daemon/keybindings/screensaver XF86ScreenSaver
+gconftool-2 --type string --set /apps/gnome_settings_daemon/keybindings/volume_down XF86AudioLowerVolume
+gconftool-2 --type string --set /apps/gnome_settings_daemon/keybindings/volume_up   XF86AudioRaiseVolume
