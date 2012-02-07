ad5a943be5958441d45ceb6b5c23608c146782a9
Thomas Adamcik <thomas@adamcik.no>
2012-02-07 15:42:18 +0100

    Switch to and configure urxvt instead of x-term

--- awesome.lua
+++ awesome.lua
@@ -15,7 +15,7 @@ require("debian.menu")
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "urxvt"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
