a6d5f2153c9f4d32fb1d59693e88548fa0963ac8
Thomas Adamcik <thomas@adamcik.no>
2013-04-09 18:18:40 +0200

    Fix i3 config location

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ for file in profile muttrc vimrc xsession zshrc XCompose Xmodmap Xresources; do
   test -f ~/.$file || ln -s $BASEDIR/$file ~/.$file
 done
 
-test -d ~/.i3        || mkdir -p ~/.i3
-test -f ~/.i3/config || ln -s $BASEDIR/i3.config ~/.i3/config
+test -d ~/.config/i3        || mkdir -p ~/.config/i3
+test -f ~/.config/i3/config || ln -s $BASEDIR/i3.config ~/.config/i3/config
 
 echo Remeber to run: aptitude install apt-file awesome bind9-host build-essential dnsutils git i3 less mosh rxvt-unicode screen vim xfonts-terminus zsh
