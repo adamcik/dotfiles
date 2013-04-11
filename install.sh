a14edf38b36531e11886567c1a97fd617b94fab7
Thomas Adamcik <thomas@adamcik.no>
2013-04-11 13:11:46 +0200

    Add i3status

--- install.sh
+++ install.sh
@@ -10,4 +10,7 @@ done
 test -d ~/.config/i3        || mkdir -p ~/.config/i3
 test -f ~/.config/i3/config || ln -s $BASEDIR/i3.config ~/.config/i3/config
 
-echo Remeber to run: aptitude install apt-file awesome bind9-host build-essential dnsutils git i3 less mosh rxvt-unicode screen vim xfonts-terminus zsh
+test -d ~/.config/i3status        || mkdir -p ~/.config/i3status
+test -f ~/.config/i3status/config || ln -s $BASEDIR/i3status.config ~/.config/i3status/config
+
+echo Remeber to run: aptitude install apt-file awesome bind9-host build-essential dnsutils git i3 ipython less mosh rxvt-unicode screen vim xfonts-terminus zsh
