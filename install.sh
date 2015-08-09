d6d80524ea9aa41d3b40b2e3bf4b492b050ea820
Thomas Adamcik <thomas@adamcik.no>
2015-08-09 23:12:22 +0200

    Update install rules to include gpg stuff

--- install.sh
+++ install.sh
@@ -19,5 +19,11 @@ test -f ~/.config/i3/config || ln -s $BASEDIR/i3.config ~/.config/i3/config
 test -d ~/.config/i3status        || mkdir -p ~/.config/i3status
 test -f ~/.config/i3status/config || ln -s $BASEDIR/i3status.config ~/.config/i3status/config
 
+test -d ~/.gnupg/ || mkdir ~/.gnupg
+test -f ~/.gnupg/gpg.conf || ln -s $BASEDIR/gpg.conf ~/.gnupg/gpg.conf
+test -f ~/.gnupg/gpg-agent.conf || ln -s $BASEDIR/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+
 echo Remember to run: aptitude install ack-grep apt-file bind9-host build-essential dnsutils git ipython keychain less mosh screen vim zsh
 echo Remember to run: aptitude install i3 rxvt-unicode xfonts-terminus redshift xautolock
+echo Remember to run: aptitude install scdaemon gnupg2 gnupg-agent libccid pinentry-curses
+echo Remember to run: gsettings set org.gnome.settings-daemon.plugins.keyboard active false
