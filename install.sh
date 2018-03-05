b157e85a13e9ccd2d6fb1435abb90f4c37d82b2d
Thomas Adamcik <thomas@adamcik.no>
2018-03-05 10:52:37 +0100

    Update recomended extras to install

--- install.sh
+++ install.sh
@@ -24,9 +24,9 @@ test -f ~/.gnupg/gpg.conf || ln -s $BASEDIR/gpg.conf ~/.gnupg/gpg.conf
 test -f ~/.gnupg/gpg-agent.conf || ln -s $BASEDIR/gpg-agent.conf ~/.gnupg/gpg-agent.conf
 test -f ~/.gnupg/sks-keyservers.netCA.pem || ln -s $BASEDIR/sks-keyservers.netCA.pem ~/.gnupg/sks-keyservers.netCA.pem
 
-echo Remember to run: aptitude install ack-grep apt-file bind9-host build-essential dnsutils git ipython keychain less mosh screen vim zsh
-echo Remember to run: aptitude install i3 rxvt-unicode xfonts-terminus redshift xautolock
-echo Remember to run: aptitude install scdaemon gnupg gnupg-agent libccid pinentry-curses gnupg-curl
+echo Remember to run: aptitude install ack-grep bind9-host dnsutils git ipython less mosh screen vim zsh
+echo Remember to run: aptitude install i3 rxvt-unicode redshift xautolock pavucontrol
+echo Remember to run: aptitude install scdaemon gnupg gnupg-agent libccid pinentry-curses dbus-user-session
 echo Remember to run: gsettings set org.gnome.settings-daemon.plugins.keyboard active false
 echo Remember to run: 'gpg2 --card-status\n fetch\n^D; gpg2 --edit-key ...\ntrust\n5\n^D'
 
