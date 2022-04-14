b22fa1e50706fc9e17d52f22780f2a5ee579dc5f
Thomas Adamcik <thomas@adamcik.no>
2022-04-14 12:11:06 +0200

    Rewrite install script

--- install.sh
+++ install.sh
@@ -6,31 +6,26 @@ BASEDIR=$(readlink -m `dirname $0`)
 # Get rid of existing profile as we want our own.
 test -f ~/.profile && test -L ~/.profile || mv ~/.profile ~/.profile.bak
 
-for file in profile muttrc vimrc zshrc Xresources gitconfig screenrc; do
-  test -f ~/.$file || ln -s $BASEDIR/$file ~/.$file
-done
+symlink() {
+test -d $(dirname $1) || mkdir -p $(dirname $1)
+test -L $1 || ln -s $BASEDIR/$2 $1
+}
 
-test -d ~/.ssh        || mkdir -p ~/.ssh
-test -f ~/.ssh/config || ln -s $BASEDIR/ssh_config ~/.ssh/config
-test -f ~/.ssh/rc     || ln -s $BASEDIR/ssh_rc ~/.ssh/rc
-test -f ~/.ssh/rc     || ln -s $BASEDIR/ssh_rc ~/.ssh/rc
-
-test -d ~/.config/i3        || mkdir -p ~/.config/i3
-test -f ~/.config/i3/config || ln -s $BASEDIR/i3.config ~/.config/i3/config
-
-test -d ~/.config/i3status        || mkdir -p ~/.config/i3status
-test -f ~/.config/i3status/config || ln -s $BASEDIR/i3status.config ~/.config/i3status/config
-
-test -d ~/.config/kitty        || mkdir -p ~/.config/kitty
-test -f ~/.config/kitty/config || ln -s $BASEDIR/kitty.conf ~/.config/kitty/kitty.conf
-
-test -d ~/.gnupg/ || mkdir ~/.gnupg && chmod 700 ~/.gnupg
-test -f ~/.gnupg/gpg.conf || ln -s $BASEDIR/gpg.conf ~/.gnupg/gpg.conf
-test -f ~/.gnupg/gpg-agent.conf || ln -s $BASEDIR/gpg-agent.conf ~/.gnupg/gpg-agent.conf
-test -f ~/.gnupg/sks-keyservers.netCA.pem || ln -s $BASEDIR/sks-keyservers.netCA.pem ~/.gnupg/sks-keyservers.netCA.pem
-
-test -d ~/.local/bin || mkdir ~/.local/bin
-test -f ~/.local/bin/input-event || ln -s $BASEDIR/input-event ~/.local/bin/input-event
+symlink ~/.config/i3/config       i3.config
+symlink ~/.config/i3status/config i3status.config
+symlink ~/.config/kitty/config    kitty.conf
+symlink ~/.gitconfig              gitconfig
+symlink ~/.gnupg/gpg-agent.conf   gpg-agent.conf
+symlink ~/.gnupg/gpg.conf         gpg.conf
+symlink ~/.local/bin/input-event  input-event
+symlink ~/.muttrc                 muttrc
+symlink ~/.profile                profile
+symlink ~/.screenrc               screenrc
+symlink ~/.ssh/config             ssh_config
+symlink ~/.ssh/rc                 ssh_rc
+symlink ~/.vimrc                  vimrc
+symlink ~/.zshrc                  zshrc
+symlink ~/.Xresources             Xresources
 
 echo Remember to run: aptitude install ack-grep bind9-host dnsutils git ipython less mosh screen vim zsh
 echo Remember to run: aptitude install i3 kitty redshift xss-lock xautolock pavucontrol kitty inputplug
