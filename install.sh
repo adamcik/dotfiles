18969dd0e9e032ded241fcb597dbdd84922a81eb
Thomas Adamcik <thomas@adamcik.no>
2024-12-25 10:50:59 +0100

    Update symlink helper
    - Proper escaping
    - Handle stray backup and not overwriting

--- install.sh
+++ install.sh
@@ -11,17 +11,23 @@ BASEDIR="$(readlink -m "$(dirname "$0")")"
 
 symlink() {
 	DIRECTORY="$(dirname "$1")"
-	TARGET="$BASEDIR/$2"
+	TARGET="${BASEDIR}/$2"
 	LINK_NAME="$1"
 	# Create directory if missing:
-	test -d "$DIRECTORY" || mkdir -p "$DIRECTORY"
+	test -d "${DIRECTORY}" || mkdir -p "${DIRECTORY}"
 
 	# Remove stray backup:
-	test -e "${LINK_NAME}.backup" -a "$(readlink "${LINK_NAME}.backup")" = "$(readlink "$TARGET")" &&
+	test -L "${LINK_NAME}.backup" -a "$(readlink "${LINK_NAME}.backup")" = "$(readlink "${LINK_NAME}")" &&
 		rm "${LINK_NAME}.backup"
 
-	printf "%-40s → %s\n" "$(realpath --relative-base="$HOME" --no-symlinks "$LINK_NAME")" "$2"
-	ln --no-dereference --symbolic --force "$TARGET" "$LINK_NAME"
+	# Backup before replacing:
+	test -L "${LINK_NAME}" ||
+		echo mv "${LINK_NAME}" "${LINK_NAME}.backup"
+
+	printf "~/%-40s → ~/%s\n" \
+		"$(realpath --relative-base="$HOME" --no-symlinks "${LINK_NAME}")" \
+		"$(realpath --relative-base="$HOME" --no-symlinks "${TARGET}")"
+	ln --no-dereference --symbolic --force "${TARGET}" "${LINK_NAME}"
 }
 
 echo Setting up links:
