3ce8d6a10634f2b171e5bf997dbf3701d6189d68
Thomas Adamcik <thomas@adamcik.no>
2024-12-28 10:12:50 +0100

    Properly export variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
-set EDITOR vim
-set LANG en_DK.utf8
-set PATH ~/.local/bin $PATH
-set MOSH_SERVER_NETWORK_TMOUT 2592000
-set PYTHON_KEYRING_BACKEND keyring.backends.null.Keyring
+set --export EDITOR vim
+set --export LANG en_DK.utf8
+set --export PATH ~/.local/bin $PATH
+set --export MOSH_SERVER_NETWORK_TMOUT 2592000
+set --export PYTHON_KEYRING_BACKEND keyring.backends.null.Keyring
 
 set fish_greeting
 
